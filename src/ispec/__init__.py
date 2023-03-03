ff4e8e7dbe0b91a5fb0889875f541c8a6f5ec1c2
Clément GUICHARD <clement.guichard0@gmail.com>
2023-03-03 23:16:49 +0100

    build: add pyproject.toml, packaging with hatch (vcs versioning)

--- src/ispec/__init__.py
+++ src/ispec/__init__.py
@@ -1,1 +1,9 @@
 """Top-level package for ispec."""
+
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("ispec")
+except PackageNotFoundError:
+    # package is not installed
+    __version__ = "undefined"
